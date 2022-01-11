74c579421c87bf386d736b5f64a41c0b449a8f42
Felipe Sales <felipe.ferreira.sales@gmail.com>
2022-01-11 18:49:28 -0300

    Ajuste do modelo

--- src/main/java/br/sales/estante/model/ArtistRole.java
+++ src/main/java/br/sales/estante/model/ArtistRole.java
@@ -1,6 +1,8 @@
 package br.sales.estante.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,16 +10,15 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "artist_role")
 public class ArtistRole extends PanacheEntity {
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "creative_team_id", referencedColumnName = "id", nullable=false)
-    private CreativeTeam creativeTeam;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "artist_id", referencedColumnName = "id", nullable=false)
+    @JoinColumn(name = "artist_id", referencedColumnName = "id")
     private Artist artist;
     private ArtistType type;
     private LocalDate date;
