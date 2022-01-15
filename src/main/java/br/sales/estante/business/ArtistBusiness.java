c5033775f64b642fbf7ec9ac9db51e1c23a63970
Felipe Sales <felipe.ferreira.sales@gmail.com>
2022-01-15 18:38:01 -0300

    Logs do crud de artista

--- src/main/java/br/sales/estante/business/ArtistBusiness.java
+++ src/main/java/br/sales/estante/business/ArtistBusiness.java
@@ -4,10 +4,10 @@ import br.sales.estante.dto.ArtistRequest;
 import br.sales.estante.model.Artist;
 import br.sales.estante.model.ArtistRole;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -18,45 +18,58 @@ import java.util.Optional;
 @ApplicationScoped
 public class ArtistBusiness {
 
+    private static final Logger LOG = Logger.getLogger(ArtistBusiness.class);
+
     public List<Artist> listAllArtists() {
+        LOG.info("[listAll] pesquisando a lista de total de artistas");
         return Artist.listAll();
     }
 
     public Artist getById(Long id) {
+        LOG.info("[getById] pesquisando artista com o id " + id);
         Optional<Artist> optionalArtist = Artist.findByIdOptional(id);
         if (optionalArtist.isEmpty()) {
+            LOG.error("[getById] artista não encontrado");
             throw new NotFoundException("Artista não encontrado");
         }
+        LOG.info("[getById] artista encontrado");
         return optionalArtist.get();
     }
 
     @Transactional
     public Artist create(ArtistRequest artistRequest) {
+        LOG.info("[create] montando objeto Artist");
         var artist = Artist.builder()
                 .name(artistRequest.getName())
                 .date(LocalDate.now())
                 .build();
-
+        LOG.info("[create] persistindo objeto Artist");
         PanacheEntityBase.persist(artist);
         return artist;
     }
 
     @Transactional
     public Artist update(Long id, ArtistRequest artistRequest) {
+        LOG.info("[update] pesquisando artista");
         var artist = getById(id);
         artist.setName(artistRequest.getName());
         artist.setDate(LocalDate.now());
+        LOG.info("[update] persistindo atualizações de artista");
         artist.persist();
         return artist;
     }
 
     @Transactional
     public void delete(Long id) {
+        LOG.info("[delete] pesquisando artista");
         var artist = getById(id);
+        LOG.info("[delete] verificando se existe algum livro cadastrdo com esse artista");
         long countRoles = ArtistRole.count("artist", artist);
         if (countRoles > 0) {
+            LOG.error("[delete] Artista cadastrado em livro em uso");
             throw new WebApplicationException("Artista cadastrado em livro em uso.", Response.Status.CONFLICT);
         }
+        LOG.info("[delete] deletando artista");
         artist.delete();
     }
 }
--- src/main/java/br/sales/estante/resources/ArtistResource.java
+++ src/main/java/br/sales/estante/resources/ArtistResource.java
@@ -6,6 +6,7 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -15,6 +16,8 @@ import javax.ws.rs.core.Response;
 @Path("/v1/artists")
 @Tag(name = "/v1/artists", description = "API de Artistas")
 public class ArtistResource {
+
+    private static final Logger LOG = Logger.getLogger(ArtistResource.class);
 
     @Inject
     private ArtistBusiness artistBusiness;
@@ -26,7 +29,8 @@ public class ArtistResource {
             @APIResponse(description = "Retorna 200 para sucesso", responseCode = "200"),
             @APIResponse(description = "retorna 404 se não encontrar nenhum", responseCode = "404")
     })
-    public Response ListAll() {
+    public Response listAll() {
+        LOG.info("[listAll] pesquisando a lista de total de artistas");
         return Response.ok(artistBusiness.listAllArtists()).build();
     }
 
@@ -39,7 +43,9 @@ public class ArtistResource {
             @APIResponse(description = "retorna 404 se não encontrar", responseCode = "404")
     })
     public Response findById(@PathParam("id") Long id) {
+        LOG.info("[findById] pesquisando artista com id " + id);
         var artist = artistBusiness.getById(id);
+        LOG.info("[findById] artista encontrado");
         return Response.ok(artist).build();
     }
 
@@ -52,7 +58,9 @@ public class ArtistResource {
             @APIResponse(description = "retorna 400 caso algum parâmetro esteja inválido", responseCode = "400")
     })
     public Response save(final ArtistRequest artistRequest) {
+        LOG.info("[save] cadastrando artista");
         var artist = artistBusiness.create(artistRequest);
+        LOG.info("[save] artista cadastrado");
         return Response.ok(artist).status(Response.Status.CREATED).build();
     }
 
@@ -67,7 +75,9 @@ public class ArtistResource {
             @APIResponse(description = "retorna 404 se não encontrar", responseCode = "404")
     })
     public Response update(@PathParam("id") Long id, final ArtistRequest artistRequest) {
+        LOG.info("[update] atualizando artista com id " + id);
         var artist = artistBusiness.update(id, artistRequest);
+        LOG.info("[update] artista atualizado");
         return Response.ok(artist).build();
     }
 
@@ -79,7 +89,9 @@ public class ArtistResource {
             @APIResponse(description = "retorna 404 se não encontrar", responseCode = "404")
     })
     public Response delete(@PathParam("id") Long id) {
+        LOG.info("[delete] deletando artista com id " + id);
         artistBusiness.delete(id);
+        LOG.info("[delete] artista deletado");
         return Response.noContent().build();
     }
 }
